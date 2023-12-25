72f8b6f8fefd3b289285b2018a4f02d6bfc37337
unknown <filipdjurdjevac12356@gmail.com>
2023-12-25 06:48:49 +0100

    first commit

--- src/Car.java
+++ src/Car.java
@@ -1,9 +1,30 @@
 
 public class Car {
+    private String make;
+    private String model;
+    private int year;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public Car() {
+        this.make = "Nepoznato";
+        this.model = "Nepoznato";
+        this.year = 2015;
+    }
 
-	}
+    public Car(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
 
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+}
 }
